03b398ccb68a0bf27bc71f958983700c98281859
DynamoClub <infodynamoclub@gmail.com>
2025-06-30 03:16:03 -0500

    Update productos.js

--- data/productos.js
+++ data/productos.js
@@ -1,11 +1,3 @@
 const productosData = [
-  {
-    "id": 1,
-    "name": "Camiseta Oficial Dynamo",
-    "description": "Camiseta de entrenamiento profesional con tecnología transpirable",
-    "price": 65000,
-    "category": "camisetas",
-    "image": "../public/img/tienda/camiseta_dynamo_2025.webp",
-    "discount": 10
-  }
+
 ];
