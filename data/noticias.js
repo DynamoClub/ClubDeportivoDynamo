1e6a8aeda9b9e721dca90b2c2f85c9dddd56c313
DynamoClub <infodynamoclub@gmail.com>
2025-07-01 02:33:27 -0500

    Update noticias.js

--- data/noticias.js
+++ data/noticias.js
@@ -25,7 +25,7 @@ const noticiasData = [
     "title": "Resultados Liga Vallecaucana - Dynamo Sub-9",
     "url": "resultados-liga-vallecaucana-dynamo-sub-9",
     "date": "2025-06-10",
-    "category": "partido",
+    "category": "Partido",
     "description": "Las categorías Sub-9 B y Sub-9 A de Dynamo se impusieron con victorias en la Liga Vallecaucana @lifutbolvalle, destacando carácter y trabajo en equipo.",
     "image": "./public/img/noticias/Dynamo Sub-9.webp",
     "content": "Hoy, 19 de junio de 2025, las categorías Sub-9 B y Sub-9 A del Club Deportivo Dynamo brillaron en la jornada de la Liga Vallecaucana @lifutbolvalle, logrando victorias que reflejan su esfuerzo y dedicación. El equipo Sub-9 B venció 4-2 a Leyendas del Valle en el Festival Futbol Base, mientras que el Sub-9 A se impuso 4-2 a Deportivo H.C. en una final emocionante. Ambos partidos destacaron la entrega de los jóvenes atletas y el compromiso de sus formadores.\n\nBajo la dirección de Cristian Espinoza (Sub-9 B) y Jhon Vasquez (Sub-9 A), los equipos mostraron un juego sólido y cohesionado. La jornada, capturada por @jsphotoograph, fue un testimonio del progreso constante de los jugadores, quienes con cada partido refuerzan el prestigio del club. Este éxito subraya la importancia del trabajo en equipo y la formación de grandes promesas para el futuro.\n\n¡Felicitaciones a los jugadores, formadores y toda la familia Dynamo por estos resultados! #formandograndespromesas"
@@ -35,7 +35,7 @@ const noticiasData = [
     "title": "Final Sub-9 A - Dynamo vs. Deportivo H.C.",
     "url": "final-sub-9-a-dynamo-vs-deportivo-hc",
     "date": "2025-06-10",
-    "category": "partido",
+    "category": "Partido",
     "description": "Dynamo Sub-9 A se coronó campeón en la final contra Deportivo H.C. con un marcador de 4-2 en la Liga Vallecaucana.",
     "image": "./public/img/noticias/Final Sub-9 A.webp",
     "content": "En una jornada memorable el 19 de junio de 2025, el equipo Sub-9 A de Dynamo se alzó con la victoria en la final de la Liga Vallecaucana contra Deportivo H.C., terminando con un marcador de 4-2. Dirigidos por el formador Jhon Vasquez, los jóvenes demostraron gran determinación y habilidad en el campo. Jugadores como Juan Cantillo, Jeremy Caravali, Kerim Lucumi y Auto Gol fueron clave en el triunfo, capturado magistralmente por @jsphotoograph.\n\nEl partido, lleno de intensidad, mostró el talento emergente de la categoría, consolidando el nombre de Dynamo como un semillero de éxito. La celebración final reflejó el apoyo incondicional de la comunidad y el esfuerzo de los formadores. ¡Un paso más hacia la grandeza con #formandograndespromesas!"
@@ -45,7 +45,7 @@ const noticiasData = [
     "title": "Programación | @lifutbolvalle - Festival",
     "url": "programacion-festival-futbol-base-21-de-junio-2025",
     "date": "2025-06-21",
-    "category": "torneo",
+    "category": "Torneo",
     "description": "La programación del Festival Futbol Base 2025 inicia el 21 de junio con partidos en varias categorías en los campos Panamericanos.",
     "image": "./public/img/noticias/festival_futbol_base_dynamo.jpg",
     "content": "La programación del Festival Futbol Base, organizado por Club Deportivo Dynamo, comienza el 21 de junio de 2025 en los campos Panamericanos. Los horarios son:\n\n- 4:00 PM / Sub 6\n- 2:00 PM / Sub 7\n- 4:00 PM / Sub 8\n- 5:00 PM / Sub 8\n- 10:00 AM / Sub 8\n- 10:00 AM / Sub 10\n- 9:00 AM / Sub 9\n\n¡Vamos Dynamo! 🖤🧡 Agradecemos a @aritex.colombia #formandograndespromesas!"
@@ -55,7 +55,7 @@ const noticiasData = [
     "title": "Programación | @lifutbolvalle - División",
     "url": "programacion-division-liga-vallecaucana-junio-23-25-2025",
     "date": "2025-06-24",
-    "category": "torneo",
+    "category": "Torneo",
     "description": "La programación de la Liga Vallecaucana de Fútbol 2025 inicia el 23 de junio con partidos en la División B en varias sedes.",
     "image": "./public/img/noticias/programacion_sub16.jpg",
     "content": "La programación de la Liga Vallecaucana de Fútbol, organizada por Club Deportivo Dynamo, comienza el 23 de junio de 2025 en la División B. Los horarios son:\n\n- 3:00 PM / Sub 16\n- 2:00 PM / Sub 15\n- 2:00 PM / Sub 14\n\n¡Vamos Dynamo! 🖤🧡 #formandograndespromesas!"
@@ -65,7 +65,7 @@ const noticiasData = [
     "title": "Resultados Liga Vallecaucana - Sub 8A",
     "url": "resultado-liga-vallecaucana-sub-8a",
     "date": "2025-06-24",
-    "category": "partido",
+    "category": "Partido",
     "description": "El equipo Sub-8A de Dynamo triunfó con un marcador de 11-4 contra Deportivo Cali en el Festival Futbol Base.",
     "image": "./public/img/noticias/Resultado Sub-8A Dynamo vs Deportivo Cali.jpg",
     "content": "En una destacada actuación el 23 de junio de 2025, el equipo Sub-8A de Dynamo venció a Deportivo Cali con un marcador de 11-4 en el Festival Futbol Base. Jugadores como Zapata, Kilyan, Balanta, Juan Se y Steven brillaron en el campo. ¡Gran esfuerzo con #formandograndespromesas! Diseñador📸 @jsphotoograph"
@@ -75,7 +75,7 @@ const noticiasData = [
     "title": "Resultados Liga Vallecaucana - Sub 8A vs Villa Real",
     "url": "resultado-liga-vallecaucana-sub-8a-vs-villa-real",
     "date": "2025-06-24",
-    "category": "partido",
+    "category": "Partido",
     "description": "El equipo Sub-8A de Dynamo ganó 5-2 contra Villa Real en el Festival Futbol Base.",
     "image": "./public/img/noticias/Resultado Sub-8A Dynamo vs Villa Real.jpg",
     "content": "El 23 de junio de 2025, el equipo Sub-8A de Dynamo se impuso a Villa Real con un marcador de 5-2 en el Festival Futbol Base. Jugadores como Zapata, Kilyan y Balanta destacaron en el partido. ¡Orgullo con #formandograndespromesas! Diseñador📸 @jsphotoograph"
@@ -85,7 +85,7 @@ const noticiasData = [
     "title": "Resultados Liga Vallecaucana - Sub 7",
     "url": "resultado-liga-vallecaucana-sub-7",
     "date": "2025-06-24",
-    "category": "partido",
+    "category": "Partido",
     "description": "El equipo Sub-7 de Dynamo logró una victoria de 13-4 contra Club FG en el Festival Futbol Base.",
     "image": "./public/img/noticias/Resultado Sub-7 Dynamo vs Club F00G.jpg",
     "content": "El 23 de junio de 2025, el equipo Sub-7 de Dynamo venció a Club FG con un impresionante marcador de 13-4 en el Festival Futbol Base. Jugadores como Matías Guerrero, Alan Murillo y Samuel Carabali fueron clave. ¡Éxito con #formandograndespromesas! Diseñador📸 @jsphotoograph"
@@ -95,7 +95,7 @@ const noticiasData = [
     "title": "7 Pasos Que Debes Tener en Cuenta 🤔",
     "url": "7-pasos-deporte-25-junio-2025",
     "date": "2025-06-25",
-    "category": "general",
+    "category": "General",
     "description": "Consejos para padres y familias sobre cómo entender el deporte como una herramienta de educación y desarrollo, no de castigo o control.",
     "image": "./public/img/noticias/7 Pasos Deporte Junio 25.jpg",
     "content": "Aquí tienes valiosos consejos para padres y familias sobre el papel del deporte en la vida de los jóvenes, destacando que no debe ser una herramienta de castigo ni de control, sino un camino para educar, liberar, construir carácter y transformar vidas. Considera estos 7 pasos:\n\n1. Promover la motivación: Anima a tu hijo a disfrutar del deporte con entusiasmo, no como una obligación.\n2. Ocupar el tiempo libre: Fomenta su práctica como una actividad positiva para llenar sus horas de ocio.\n3. Cuidar la salud: Enseña a tu familia que el ejercicio regular fortalece el cuerpo y la mente.\n4. Desarrollar valores: Usa el deporte para inculcar respeto, trabajo en equipo y perseverancia en casa.\n5. Fomentar habilidades sociales: Crea oportunidades donde tu hijo aprenda a relacionarse y colaborar con otros.\n6. Impulsar el crecimiento emocional: Ayuda a tu hijo a gestionar emociones y construir resiliencia a través del juego.\n7. Educar con diálogo: Establece límites y consecuencias mediante la comunicación, evitando el castigo.\n\nSigue estos consejos para apoyar el desarrollo integral de tus hijos con #formandograndesproemsas y #escueladepadres!"
@@ -105,7 +105,7 @@ const noticiasData = [
     "title": "Club Deportivo Dynamo | 🤝 @socratesvalenciafc",
     "url": "evento-deportivo-dynamo-vs-socratesfc-30-junio-2025",
     "date": "2025-06-30",
-    "category": "evento",
+    "category": "Evento",
     "description": "El Club Deportivo Dynamo llevó a 50 deportistas de las categorías Sub-11, Sub-12 y Sub-13 Elites a una jornada deportiva en Pereira, en la sede de Sócrates Fútbol Club.",
     "image": "./public/img/noticias/Evento Dynamo vs SocratesFC Junio 30.jpg",
     "content": "El 30 de junio de 2025, el Club Deportivo Dynamo envió una delegación de 50 deportistas de las categorías Sub-11, Sub-12 y Sub-13 Elites a una jornada deportiva en Pereira, realizada en la sede deportiva de Sócrates Fútbol Club. Este encuentro fortaleció lazos para futuros proyectos y destacó el compromiso del club con el desarrollo de sus talentos. ¡Seguimos #formandoGrandespromesas!"
