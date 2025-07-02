55b93057452d819f2e254a4bfb3231df2324d886
DynamoClub <infodynamoclub@gmail.com>
2025-07-01 19:27:22 -0500

    Update productos.js

--- data/productos.js
+++ data/productos.js
@@ -1,3 +1,47 @@
 const productosData = [
-
+  {
+    "id": 1,
+    "name": "Uniforme Oficial Dynamo - Equipación 1",
+    "description": "Equipación de jugador profesional",
+    "price": 0,
+    "category": "equipamiento",
+    "image": "../public/img/tienda/uniforme_dynamo.png",
+    "discount": 0
+  },
+  {
+    "id": 2,
+    "name": "Uniforme Oficial Dynamo - Equipación 2",
+    "description": "Equipación de jugador profesional",
+    "price": 0,
+    "category": "equipamiento",
+    "image": "../public/img/tienda/uniforme_dynamo_2.png",
+    "discount": 0
+  },
+  {
+    "id": 3,
+    "name": "Uniforme Oficial Dynamo - Equipación 3",
+    "description": "Equipación de jugador profesional",
+    "price": 0,
+    "category": "equipamiento",
+    "image": "../public/img/tienda/uniforme_dynamo_3.png",
+    "discount": 0
+  },
+    {
+    "id": 4,
+    "name": "Uniforme Oficial Dynamo - Equipación 4",
+    "description": "Equipación de jugador profesional",
+    "price": 0,
+    "category": "equipamiento",
+    "image": "../public/img/tienda/uniforme_dynamo_4.png",
+    "discount": 0
+  },
+  {
+    "id": 5,
+    "name": "Uniforme Oficial Dynamo - Equipación Arquero",
+    "description": "Equipación de jugador profesional",
+    "price": 0,
+    "category": "equipamiento",
+    "image": "../public/img/tienda/uniforme_dynamo_arquero.png",
+    "discount": 0
+  }
 ];
