fe59092905c65177eef62a46a4a0a06fa37033ab
DynamoClub <infodynamoclub@gmail.com>
2025-07-01 19:35:10 -0500

    Update galeria.js

--- data/galeria.js
+++ data/galeria.js
@@ -1,6 +1,4 @@
-// data/galeria.js
 const galeriaData = [
-    // Imágenes
     {
         "id": 1,
         "type": "image",
@@ -281,7 +279,6 @@ const galeriaData = [
         "alt": "Dynamo Recuerdos: Dos arqueros en azul posando juntos en la cancha",
         "title": "Dúo de Guardianes" 
     },
-    // Videos
     {   
         "id": 47,
         "type": "video",
@@ -303,31 +300,28 @@ const galeriaData = [
         "poster": "../public/img/videos/portada-2-mp4.png",
         "alt": "Dynamo Recuerdos: Jugadores recibiendo reconocimientos en una entrega de premios",
         "title": "Entrega de Reconocimientos"
-    }
-        
-    // Agrega más elementos aquí siguiendo el mismo patrón
-    // Para agregar una nueva imagen:
-    // {
-    //     id: NUMERO_UNICO,
-    //     type: 'image',
-    //     src: 'ruta/a/tu/imagen.jpg',
-    //     alt: 'Descripción de la imagen',
-    //     orientation: 'horizontal' o 'vertical',
-    //     title: 'Título de la imagen'
-    // }
-    
-    // Para agregar un nuevo video:
-    // {
-    //     id: NUMERO_UNICO,
-    //     type: 'video',
-    //     src: {
-    //         mp4: 'ruta/a/tu/video.mp4',
-    //         webm: 'ruta/a/tu/video.webm'
-    //     },
-    //     poster: 'ruta/a/thumbnail.jpg',
-    //     orientation: 'horizontal' o 'vertical',
-    //     title: 'Título del video'
-    // }
+    },
+        {
+        "id": 49,
+        "type": "image",
+        "src": "../public/img/galeria/49.jpg",
+        "alt": "Dynamo Recuerdos: Victoria Dynamo, en Torneo Nacional Sub 15",
+        "title": "Torneo nacional sub-15" 
+    },
+    {
+        "id": 50,
+        "type": "image",
+        "src": "../public/img/galeria/50.jpg",
+        "alt": "Dynamo Recuerdos: Victoria Dynamo, en Competencia BOM BOM BUM",
+        "title": "Competencia: BOM BOM BUM Sub-9A" 
+    },
+    {
+        "id": 51,
+        "type": "image",
+        "src": "../public/img/galeria/51.jpg",
+        "alt": "Dynamo Recuerdos: Victoria Dynamo, Sub-14 Division B",
+        "title": "Victoria Sub-14, División B" 
+    },
 ];
 
 // Exportar los datos para uso en otros archivos
