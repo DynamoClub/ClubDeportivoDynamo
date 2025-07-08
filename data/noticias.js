0a03b1122ba0538904d49c070b26edd9abf61976
DynamoClub <infodynamoclub@gmail.com>
2025-07-08 07:54:36 -0500

    Update noticias.js

--- data/noticias.js
+++ data/noticias.js
@@ -150,4 +150,14 @@ const noticiasData = [
     "image": "./public/img/noticias/sub_14_pitazo_final_dynamo.jpg",
     "content": "𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗗𝗢𝗦 𝗟𝗜𝗚𝗔 𝗩𝗔𝗟𝗟𝗘𝗖𝗔𝗨𝗖𝗔𝗡𝗔 ⚽🔥\n\nNuestra Categoría Sub-14 logró una importante victoria en la Liga Vallecaucana tras un vibrante partido frente a la cantera del Deportivo Cali.\n\n🏆 Resultado: Dynamo 4️⃣ - 3️⃣ Deportivo Cali Cantera B\n\n📋 DT: Oscar Aaron Lucumí\n\n⚽ Goleadores:\n- Samuel Vásquez ⚽⚽⚽\n- Preciado ⚽\n\n👏 Gran esfuerzo y entrega de todo el equipo. ¡A seguir así!\n\n📸 Diseñador: @jsphotoograph\n\n#formandograndespromesas 🧡🖤"
   },
+    {
+  "id": "16",
+  "title": "Inscripciones Abiertas | Club Deportivo Dynamo ⚽🔥",
+  "url": "inscripciones-abiertas-club-dynamo-6-julio-2025",
+  "date": "2025-07-06",
+  "category": "Inscripción",
+  "description": "¡Haz parte de la familia Dynamo! Entrenamientos para todas las edades y participaciones en torneos nacionales e internacionales.",
+  "image": "./public/img/noticias/inscripciones_abiertas_dynamo.jpg",
+  "content": "🔥⚽ ¡INSCRIPCIONES ABIERTAS! ⚽🔥\n\nSEDE: Ciudad Córdoba\nSEDE: Vegas de Comfandi\n\n¿Te apasiona el fútbol y quieres llevar tu talento al siguiente nivel? En Club Deportivo Dynamo te estamos buscando 💪\n\n🎯 Entrenamientos profesionales\n👟 Formación desde los más pequeños hasta categorías mayores\n🌟 Ambientes seguros, motivadores y de alto rendimiento\n🏆 Participación en torneos y competencias nacionales e internacionales.\n\n👉 <a href=\"https://wa.me/573126723778?text=Hola%2C%20me%20gustaría%20saber%20el%20proceso%20de%20inscribir%20a%20mi%20niño.\" target=\"_blank\">Haz clic aquí para escribirnos por WhatsApp</a>\n\n📑 O si lo prefieres, visita nuestra página web y llena el formulario en la sección de contacto para que uno de nuestros asesores se comunique contigo.\n\n📸 Diseñador: @jsphotoograph\n\n#formandograndespromesas 🧡🖤 #dynamoespasión #FútbolConPropósito #InscripcionesAbiertas #clubdynamo #SueñaJuegaGana #publicidad #futbol"
+},
 ];
