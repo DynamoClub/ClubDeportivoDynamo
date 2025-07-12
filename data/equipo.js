3eda4b16454a93bd9e94310598d95ce368a0922a
DynamoClub <infodynamoclub@gmail.com>
2025-07-11 19:30:51 -0500

    Update equipo.js

--- data/equipo.js
+++ data/equipo.js
@@ -2,21 +2,21 @@ const equipoData = [
   {
     "id": "1",
     "name": "Oscar Aaron Lucumi",
-    "role": "Entrenador",
+    "role": "Directivo",
     "description": "Oscar Aaron Lucumi es un profesional apasionado con experiencia en formación de jóvenes talentos y trayectoria como exjugador profesional. Con estudios en Tecnólogo en Mercadeo y Negocios Internacionales, Administración y Legislación Deportiva, y Licencia C, ha jugado en clubes como Deportivo Cali, Córdoba F.C., Club Atlético La Sabana y Expreso Rojo. Como formador de la categoría Sub-15, se dedica a inculcar disciplina y respeto en los jóvenes atletas.",
     "image": "../public/img/equipo/AaronEntreneador.webp"
   },
   {
     "id": "2",
     "name": "David Andrade",
-    "role": "Entrenador",
+    "role": "Directivo",
     "description": "David Andrade es un formador con una sólida trayectoria académica y profesional, comprometido con el desarrollo técnico y táctico de los jugadores. Posee estudios en Administración y Legislación Deportiva, Tecnólogo en Entrenamiento Deportivo, Profesional en Administración de Empresas, Especialista en Dirección y Gestión Deportiva, y Especialista en Proyectos de Desarrollo. Dirige la categoría Sub-14 en el Club Deportivo Dynamo, enfocándose en el crecimiento integral de los jóvenes.",
     "image": "../public/img/equipo/DavidEntrenador.webp"
   },
   {
     "id": "3",
     "name": "Deymer Vásquez",
-    "role": "Entrenador",
+    "role": "Directivo",
     "description": "Deymer Vásquez es un formador comprometido con la formación integral de los jóvenes atletas, inculcando valores de dedicación y trabajo en equipo. Con estudios en Administración y Legislación Deportiva, Tecnología en Gestión Deportiva (en curso) y Licencia C, cuenta con experiencia como exjugador profesional en Expreso Rojo y Once Caldas S.A. Actualmente dirige la categoría Sub-16 en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/DeymerEntrenador.webp"
   },
@@ -34,67 +34,95 @@ const equipoData = [
     "description": "Álvaro Folleco es un profesional dedicado a la gestión administrativa y financiera, asegurando el correcto funcionamiento del Club Deportivo Dynamo. Con estudios en Administración de Empresas, coordina las operaciones administrativas y financieras del club, garantizando una operación fluida y organizada.",
     "image": "../public/img/equipo/AlvaroCoordinador.webp"
   },
+    {
+  "id": "6",
+  "name": "Johan Hurtado",
+  "role": "Diseñador / Fotógrafo",
+  "description": "Johan Hurtado, Tecnólogo en Gestión Deportiva y talentoso diseñador, aporta creatividad, identidad y profesionalismo a la imagen institucional de Club Deportivo Dynamo. Su compromiso permite proyectar con fuerza el espíritu y los valores del club.",
+  "image": "../public/img/equipo/JohanHurtadoDiseñador.png"
+},
   {
-    "id": "6",
+    "id": "7",
     "name": "Luis Eduardo Vargas",
     "role": "Preparador de Arqueros",
     "description": "Luis Eduardo Vargas es un experimentado preparador de arqueros dedicado a formar y perfeccionar las habilidades de los porteros. Con estudios en Técnico en Recreación y Deporte, y experiencia como exjugador en Escuela de Fútbol Sarmiento Lora y Cortuluá FC, ha dirigido en clubes como Real Valencia, Stronger Internacional y Club Deportivo Dynamo, enfocándose en mejorar reflejos y técnicas específicas.",
     "image": "../public/img/equipo/EduardoVargasEntrenador.webp"
   },
   {
-    "id": "7",
+    "id": "8",
     "name": "Jhon Jairo Vasquez",
     "role": "Entrenador",
     "description": "Jhon Jairo Vásquez es un formador apasionado por el desarrollo integral de los jóvenes futbolistas, aportando experiencia y dedicación en las categorías base. Con Licencia B, ha dirigido en Sporting Córdoba y Club Deportivo Dynamo, donde forma las categorías Sub-07 A y Sub-08 A.",
     "image": "../public/img/equipo/JhonJairoVasquezEntrenador.webp"
   },
   {
-    "id": "8",
+    "id": "9",
     "name": "Liujohan Millan",
     "role": "Entrenador",
     "description": "Liujohan Millan es un formador comprometido con la formación física y nutricional de los deportistas, enfocándose en la creación de hábitos saludables. Con estudios en Profesional en Deporte, Nutrición y Licencia C, dirige las categorías Sub-12 B y Sub-16 B en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/LiujohanEntrenador.webp"
   },
   {
-    "id": "9",
+    "id": "10",
     "name": "Gustavo Cajiao",
     "role": "Entrenador",
     "description": "Gustavo Cajiao es un formador especializado en actividad física y educación deportiva, comprometido con el desarrollo integral de los jóvenes deportistas. Con estudios en Tecnólogo en Actividad Física, Licenciatura en Ciencias del Deporte y Educación Física (en curso) y Licencia C, dirige las categorías Sub-11 A y Sub-11 B en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/GustavoEntrenador.webp"
   },
   {
-    "id": "10",
+    "id": "11",
     "name": "Jhoan Steven Porras",
     "role": "Entrenador",
     "description": "Jhoan Steven Porras es un formador dedicado al desarrollo de jóvenes talentos, enfocado en brindar entrenamiento integral para el éxito deportivo. Con Licencia C, dirige la categoría Sub-10 B en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/JohanFormador.webp"
   },
   {
-    "id": "11",
+    "id": "12",
     "name": "Breiner Ortiz",
     "role": "Entrenador",
     "description": "Breiner Ortiz es un entrenador dedicado con una trayectoria destacada como exjugador profesional. Con Licencia C (El Salvador) y experiencia en clubes como Deportes Quindío, North Carolina (USA), Deportivo Titan y Deportivo Once Lobos (El Salvador), dirige las categorías Sub-10 y Sub-13 en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/BreinerOrtizFormador.webp"
   },
   {
-    "id": "12",
+    "id": "13",
     "name": "Juan Diego Sarria",
     "role": "Entrenador",
     "description": "Juan Diego Sarria es un formador apasionado por el deporte y comprometido con el desarrollo integral de jóvenes talentos. Con estudios en Tecnólogo en Deporte, dirige la categoría Sub-09 en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/JuanDiegoSarriaEntrenador.webp"
   },
   {
-    "id": "13",
+    "id": "14",
     "name": "Yeison Mosquera",
     "role": "Entrenador",
-    "description": "Yeison Mosquera es Técnico en Entrenamiento y Preparación Física, encargado de formar y dirigir la categoría Sub-06 en el Club Deportivo Dynamo. Comprometido con el desarrollo técnico, formativo y personal de los más pequeños del club.",
+    "description": "Yeison Mosquera es un formador entusiasta y dedicado al desarrollo integral de los jóvenes deportistas. Con estudios en Técnico en Entr Hilda de Mar y Entrenamiento y Preparación Física, dirige la categoría Sub-06 en el Club Deportivo Dynamo.",
     "image": "../public/img/equipo/YeisonMosqueraEntrenador.webp"
   },
     {
-  "id": "14",
-  "name": "Joan  Hurtado",
-  "role": "Diseñador / Fotógrafo",
-  "description": "Joan Hurtado, Tecnólogo en Gestión Deportiva y talentoso diseñador, aporta creatividad, identidad y profesionalismo a la imagen institucional de Club Deportivo Dynamo. Su compromiso permite proyectar con fuerza el espíritu y los valores del club.",
-  "image": "../public/img/equipo/JohanHurtadoDiseñador.png"
-}
+    "id": "15",
+    "name": "Cristian David Espinoza",
+    "role": "Entrenador",
+    "description": "Cristian David Espinoza es un apasionado formador del Club Deportivo Dynamo. Con estudios en Entrenamiento y Preparación Física, actualmente cursa cuarto semestre de Administración de Empresas. Dirige con entrega y liderazgo la categoría Sub-09B, motivando el desarrollo integral de los niños deportistas.",
+    "image": "../public/img/equipo/CristianEspinozaEntrenador.png"
+  },
+  {
+    "id": "16",
+    "name": "Santiago Vivanco Rodríguez",
+    "role": "Entrenador",
+    "description": "Santiago Vivanco Rodríguez es egresado de la Escuela Nacional del Deporte como Profesional en Deporte. Actualmente se desempeña como director técnico de las categorías Sub-10 y Sub-07 en el Club Deportivo Dynamo, aportando conocimiento, metodología y compromiso en la formación de nuevos talentos.",
+    "image": "../public/img/equipo/SantiagoVivancoEntrenador.png"
+  },
+  {
+    "id": "17",
+    "name": "Jerónimo Baquero Toro",
+    "role": "Entrenador Asistente",
+    "description": "Jerónimo Baquero Toro es estudiante de segundo semestre de Profesional en Deporte. Forma parte del cuerpo técnico del Club Deportivo Dynamo como asistente técnico de la categoría Sub-07, destacándose por su compromiso, disposición y proyección como futuro entrenador.",
+    "image": "../public/img/equipo/JeronimoBaqueroEntrenador.png"
+  },
+  {
+    "id": "18",
+    "name": "Willinton Andrés Rentería Riascos",
+    "role": "Entrenador Asistente",
+    "description": "Willinton Andrés Rentería Riascos es Técnico en Especialidad Deportiva y Recreativa. Hace parte del staff del Club Deportivo Dynamo como formador asistente de la categoría Sub-10C sede Vegas, demostrando vocación de servicio, carisma y liderazgo en su labor pedagógica.",
+    "image": "../public/img/equipo/WillintonRenteriaEntrenador.png"
+  }
 ];
