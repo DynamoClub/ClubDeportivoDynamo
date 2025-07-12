97585d46adf4fbf73145f2c4471b92b3d6c76064
DynamoClub <infodynamoclub@gmail.com>
2025-07-11 19:54:10 -0500

    Update jugadores.js

--- data/jugadores.js
+++ data/jugadores.js
@@ -5,7 +5,7 @@ const jugadoresData = [
     "position": "Delantero",
     "club": "Atlético Nacional",
     "description": "Julián Quevedo, conocido como 'La Araña', es un delantero letal formado en las categorías menores del Club Deportivo Dynamo. Su olfato goleador, velocidad y capacidad de definición lo han convertido en una de las grandes promesas del fútbol colombiano. Actual jugador de Atlético Nacional, ha dejado huella en el Torneo Nacional Sub-15 firmando tripletas decisivas y destacando como máximo artillero. Su esfuerzo y talento lo llevaron a vestir la camiseta de la Selección Colombia Sub-15, participando en su primer microciclo nacional. Orgullo de la casa, goleador y campeón con la Selección Valle, Quevedo representa el sello Dynamo en el fútbol profesional.",
-    "image": "../public/img/jugadores/julian-quevedo.webp"
+    "image": "../public/img/jugadores/julian-quevedo.jpg"
   },
   {
     "id": "2",
@@ -13,7 +13,7 @@ const jugadoresData = [
     "position": "Delantero",
     "club": "Atlético Huila",
     "description": "Juan Camilo Cardona es un veloz y desequilibrante extremo formado en el Club Deportivo Dynamo. Actualmente defiende los colores de Atlético Huila, consolidándose como titular en la categoría Sub-17 gracias a su alto nivel competitivo. Recientemente anotó en la victoria 3-0 frente a Fortaleza en el Torneo Nacional, confirmando su talento y proyección. Dynamo celebra el crecimiento de esta joven promesa que sigue dejando su huella en el fútbol colombiano.",
-    "image": "../public/img/jugadores/camilo-cardona.webp"
+    "image": "../public/img/jugadores/camilo-cardona.jpg"
   },
   {
     "id": "3",
@@ -21,7 +21,7 @@ const jugadoresData = [
     "position": "Delantero",
     "club": "Atlético Huila",
     "description": "Jhonier Amu, apodado 'Búfalo', es un potente delantero formado en Club Deportivo Dynamo. Actualmente hace parte de Atlético Huila Sub-17A, donde dejó su firma en las redes al anotar su primer gol de la temporada en el Torneo Nacional frente a Llaneros. Reconocido por su esfuerzo, disciplina y constancia, Jhonier es un ejemplo de superación y proyección para las nuevas generaciones Dynamo.",
-    "image": "../public/img/jugadores/jhonier-amu.webp"
+    "image": "../public/img/jugadores/jhonier-amu.jpg"
   },
   {
     "id": "4",
@@ -29,7 +29,7 @@ const jugadoresData = [
     "position": "Delantero",
     "club": "Independiente Santa Fe",
     "description": "Johnny González es un talentoso delantero formado en Club Deportivo Dynamo, actualmente figura en Independiente Santa Fe Sub-20A. Conocido por su capacidad goleadora y calidad técnica, firmó recientemente un doblete de lujo ante La Equidad, demostrando que el proceso y la disciplina dan frutos. En Dynamo celebramos con orgullo el ascenso de esta joven promesa al fútbol de alto nivel.",
-    "image": "../public/img/jugadores/johnny-gonzalez.webp"
+    "image": "../public/img/jugadores/johnny-gonzalez.jpg"
   },
   {
     "id": "5",
@@ -37,6 +37,46 @@ const jugadoresData = [
     "position": "Defensa",
     "club": "Independiente Santa Fe",
     "description": "Felipe Zuñiga, lateral izquierdo con proceso formativo en Club Deportivo Dynamo, ha logrado debutar con gol en las filas de Independiente Santa Fe Sub-20B. Destacado por su despliegue, solidez defensiva y aporte ofensivo, Felipe se perfila como una de las nuevas caras del fútbol nacional. Dynamo se enorgullece de ver crecer a este talento formado en casa.",
-    "image": "../public/img/jugadores/felipe-zuniga.webp"
-  }
+    "image": "../public/img/jugadores/felipe-zuniga.jpg"
+  },
+  {
+  "id": "6",
+  "name": "Dilan Bonilla",
+  "position": "Por definir",
+  "club": "Alianza Valledupar",
+  "description": "Dilan Bonilla, delantero con formación en las categorías juveniles del Club Deportivo Dynamo, se destaca actualmente en la categoría 2008 de Alianza Valledupar. Reconocido por su velocidad, desequilibrio y capacidad goleadora, sigue dejando en alto el sello Dynamo en el fútbol nacional juvenil.",
+  "image": "../public/img/jugadores/dilan-bonilla.jpg"
+},
+{
+  "id": "7",
+  "name": "Imanol Quiñonez",
+  "position": "Por definir",
+  "club": "Por definir",
+  "description": "Imanol Quiñonez, jugador con proceso formativo en el Club Deportivo Dynamo, ha tenido el honor de integrar convocatorias de la Selección Colombia Juvenil. Talento nacional con sello Dynamo.",
+  "image": "../public/img/jugadores/imanol-quinonez.jpg"
+},
+{
+  "id": "8",
+  "name": "Alexis Caravali",
+  "position": "Por definir",
+  "club": "Por definir",
+  "description": "Alexis Caravali, formado en las divisiones inferiores del Club Deportivo Dynamo, fue convocado a procesos de Selección Colombia Juvenil, destacándose por su disciplina y potencial competitivo.",
+  "image": "../public/img/jugadores/alexis-caravali.jpg"
+},
+{
+  "id": "9",
+  "name": "Diego Mancilla",
+  "position": "Por definir",
+  "club": "Por definir",
+  "description": "Diego Mancilla, joven promesa surgida del Club Deportivo Dynamo, integró la Selección Colombia Juvenil, dejando huella por su entrega y calidad en los procesos nacionales.",
+  "image": "../public/img/jugadores/diego-mancilla.jpg"
+},
+{
+  "id": "10",
+  "name": "Sebastián Ocampo",
+  "position": "Por definir",
+  "club": "Por definir",
+  "description": "Sebastián Ocampo, futbolista con formación en Club Deportivo Dynamo, participó en convocatorias de Selección Colombia Juvenil, reflejando el trabajo formativo del club a nivel nacional.",
+  "image": "../public/img/jugadores/sebastian-ocampo.jpg"
+}
 ];
