54f2a48104389c315e464461a133f3c920021cec
DynamoClub <infodynamoclub@gmail.com>
2025-07-11 23:40:05 -0500

    Update jugadores.js

--- data/jugadores.js
+++ data/jugadores.js
@@ -50,32 +50,32 @@ const jugadoresData = [
 {
   "id": "7",
   "name": "Imanol Quiñonez",
-  "position": "Por definir",
-  "club": "Por definir",
+  "position": "",
+  "club": "N/A",
   "description": "Imanol Quiñonez, jugador con proceso formativo en el Club Deportivo Dynamo, ha tenido el honor de integrar convocatorias de la Selección Colombia Juvenil. Talento nacional con sello Dynamo.",
   "image": "../public/img/jugadores/imanol-quinonez.jpg"
 },
 {
   "id": "8",
   "name": "Alexis Caravali",
-  "position": "Por definir",
-  "club": "Por definir",
+  "position": "",
+  "club": "N/A",
   "description": "Alexis Caravali, formado en las divisiones inferiores del Club Deportivo Dynamo, fue convocado a procesos de Selección Colombia Juvenil, destacándose por su disciplina y potencial competitivo.",
   "image": "../public/img/jugadores/alexis-caravali.jpg"
 },
 {
   "id": "9",
   "name": "Diego Mancilla",
-  "position": "Por definir",
-  "club": "Por definir",
+  "position": "",
+  "club": "N/A",
   "description": "Diego Mancilla, joven promesa surgida del Club Deportivo Dynamo, integró la Selección Colombia Juvenil, dejando huella por su entrega y calidad en los procesos nacionales.",
   "image": "../public/img/jugadores/diego-mancilla.jpg"
 },
 {
   "id": "10",
   "name": "Sebastián Ocampo",
-  "position": "Por definir",
-  "club": "Por definir",
+  "position": "",
+  "club": "N/A",
   "description": "Sebastián Ocampo, futbolista con formación en Club Deportivo Dynamo, participó en convocatorias de Selección Colombia Juvenil, reflejando el trabajo formativo del club a nivel nacional.",
   "image": "../public/img/jugadores/sebastian-ocampo.jpg"
 }
